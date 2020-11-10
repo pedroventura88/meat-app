45ba7ace07d7d35ce5d193bf38efcbbff36bd954
pedro.pasv <paventura@stefanini.com>
2020-11-10 19:44:19 -0300

    Creation of radioOption Component, responsible for show payments options.

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from '../shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  /*Esse array de RadioOptions, será passado para o radio.component.html, através do Property Binding [options]="paymentOptions que está
+   no order.component.html "*/
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão de Débito', value: 'DEB'},
+    {label: 'Cartão Refeição', value: 'REF'}
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
